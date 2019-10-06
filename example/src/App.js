d4abca7867f640fcdddce01b0e584cba137469b6
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2019-10-06 20:59:42 +0300

    Added example for account kit library

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'account-kit-react'
+import AccountKit, {AccountKitLoginType} from 'account-kit-react'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <p>Valid initialization id sequence but app is not registered in FB</p>
+        <AccountKit
+          appId='123'
+          version='v1.1'
+          onResponse={res => {
+            console.log(res)
+          }}
+          csrf={'fdhuy7721-46d7-1937-2556-da8c134e45eb'}
+          countryCode={'+7'}
+          phoneNumber={''}
+          loginType={AccountKitLoginType.PHONE}
+          language='ru_RU'
+        >
+          {({onClick, err, initialized}) => (<button onClick={onClick} disabled={!initialized || err}>Phone auth</button>)}
+        </AccountKit>
       </div>
     )
   }
